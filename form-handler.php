e2384d38bc8d39348efda7f4d32cf8b67d9aeed0
SinWraith <ronanmanuel85@gmail.com>
2023-03-17 14:22:06 +0800

    changed email
    email changed

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'Black Market Apparels';
+$email_from = 'https://sinwraith.github.io/';
 
 $email_subject = 'New Form Submission';
 
